a1889e34d34c798a4c32257be4a6ded1c06e29a5
ilmartoo <martinszga@gmail.com>
2023-05-30 11:19:57 +0200

    Backup commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'markdown-test';
+  title = 'Prosemirror Demo';
+
+  initialData =
+    '<p> Normal </p>' +
+    '<p> <strong>Bold</strong> </p>' +
+    '<p> <em>Italic</em> </p>' +
+    '<p> <code>Inline code</code> </p>' +
+    '<p> <a href="example.com">Link</a> </p>' +
+    '<h1>Header 1</h1>' +
+    '<h2>Header 2</h2>' +
+    '<p>Paragraph</p>' +
+    '<ol><li>Ordered list</li></ol>' +
+    '<ul><li>Unordered list</li></ul>' +
+    '<pre><code>Code block</code></pre>' +
+    '<blockquote>Quote</blockquote>' +
+    '<p> <img alt="Example image" src="https://picsum.photos/seed/picsum/200/300"/> </p>';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {EditorComponent} from '@tinymce/tinymce-angular';
+import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
+import { RichTextEditorComponent } from './rich-text-editor/rich-text-editor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RichTextEditorComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+	imports: [
+		BrowserModule,
+		EditorComponent,
+		CKEditorModule
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
